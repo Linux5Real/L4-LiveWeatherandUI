67420042b8f844a771e1eb284aa0fa4ff445647f
Linux4real <83520385+Linux5Real@users.noreply.github.com>
2024-10-27 02:52:58 +0100

    Add files via upload

--- L4-RealWeatherandUI/html/script.js
+++ L4-RealWeatherandUI/html/script.js
@@ -8,8 +8,23 @@ window.addEventListener('message', function(event) {
         uiElement.classList.remove('top-left', 'top-right', 'center');
         uiElement.classList.add(data.uiPosition);
 
+        const cityElement = document.getElementById('city');
+        const timeElement = document.getElementById('time');
+        const dateElement = document.getElementById('date');
+        const weatherElement = document.getElementById('weather');
+        const weatherIcon = document.getElementById('weather-icon'); 
+        const weatherText = document.getElementById('weather-text'); 
+        const separators = document.querySelectorAll('#separator');
+
+        if (data.showCity) {
+            cityElement.innerText = data.city;
+            cityElement.style.display = 'block';
+        } else {
+            cityElement.innerText = '';
+            cityElement.style.display = 'none';
+        }
+
         if (data.showTime) {
-            const timeElement = document.getElementById('time');
             if (timeElement.innerText !== data.time) {
                 timeElement.classList.add('time-update');
                 setTimeout(() => {
@@ -17,79 +32,100 @@ window.addEventListener('message', function(event) {
                     timeElement.classList.remove('time-update');
                 }, 250);
             }
+            timeElement.style.display = 'block';
         } else {
-            document.getElementById('time').innerText = '';
+            timeElement.innerText = '';
+            timeElement.style.display = 'none';
         }
 
         if (data.showDate) {
-            document.getElementById('date').innerText = data.date;
+            dateElement.innerText = data.date;
+            dateElement.style.display = 'block';
         } else {
-            document.getElementById('date').innerText = '';
+            dateElement.innerText = '';
+            dateElement.style.display = 'none';
         }
 
+        
         if (data.showWeather) {
-            document.getElementById('weather-text').innerText = data.weather;
-            let iconClass = '';
-            switch (data.weather.toLowerCase()) {
-                case 'klar':
-                    iconClass = 'fas fa-sun';
-                    break;
-                case 'sehr sonnig':
-                    iconClass = 'fas fa-sun';
-                    break;
-                case 'bewölkt':
-                    iconClass = 'fas fa-cloud';
-                    break;
-                case 'regen':
-                    iconClass = 'fas fa-cloud-showers-heavy';
-                    break;
-                case 'schnee':
-                    iconClass = 'fas fa-snowflake';
-                    break;
-                case 'nebelig':
-                    iconClass = 'fas fa-smog';
-                    break;
-                case 'gewitter':
-                    iconClass = 'fas fa-bolt';
-                    break;
-                case 'smog':
-                    iconClass = 'fas fa-smog';
-                    break;
-                case 'sandsturm':
-                    iconClass = 'fas fa-wind';
-                    break;
-                case 'asche':
-                    iconClass = 'fas fa-volcano';
-                    break;
-                case 'schneeregen':
-                    iconClass = 'fas fa-cloud-meatball';
-                    break;
-                case 'schneesturm':
-                    iconClass = 'fas fa-snowflake';
-                    break;
-                case 'aufklarend':
-                    iconClass = 'fas fa-cloud-sun';
-                    break;
-                case 'stark bewölkt':
-                    iconClass = 'fas fa-cloud';
-                    break;
-                case 'halloween':
-                    iconClass = 'fas fa-ghost';
-                    break;
-                default:
-                    const hours = parseInt(data.time.split(':')[0]);
-                    if (hours >= 6 && hours < 18) {
-                        iconClass = 'fas fa-sun';
-                    } else {
-                        iconClass = 'fas fa-moon';
-                    }
-                    break;
-            }
-            document.getElementById('weather-icon').className = iconClass;
+            weatherIcon.classList.add('fade-out');
+            weatherText.classList.add('fade-out');
+
+            setTimeout(() => {
+                document.getElementById('weather-text').innerText = data.weather;
+                let iconClass = '';
+                const currentHour = new Date().getHours();
+                const isDayTime = currentHour >= 6 && currentHour < 18; 
+
+                switch (data.weather.toLowerCase()) {
+                    case 'klar':
+                        iconClass = isDayTime ? 'fas fa-sun' : 'fas fa-moon';
+                        break;
+                    case 'sehr sonnig':
+                        iconClass = isDayTime ? 'fas fa-sun' : 'fas fa-moon';
+                        break;
+                    case 'bewölkt':
+                        iconClass = 'fas fa-cloud';
+                        break;
+                    case 'regen':
+                        iconClass = 'fas fa-cloud-showers-heavy';
+                        break;
+                    case 'schnee':
+                        iconClass = 'fas fa-snowflake';
+                        break;
+                    case 'nebelig':
+                        iconClass = 'fas fa-smog';
+                        break;
+                    case 'gewitter':
+                        iconClass = 'fas fa-bolt';
+                        break;
+                    case 'smog':
+                        iconClass = 'fas fa-smog';
+                        break;
+                    case 'sandsturm':
+                        iconClass = 'fas fa-wind';
+                        break;
+                    case 'asche':
+                        iconClass = 'fas fa-volcano';
+                        break;
+                    case 'schneeregen':
+                        iconClass = 'fas fa-cloud-meatball';
+                        break;
+                    case 'schneesturm':
+                        iconClass = 'fas fa-snowflake';
+                        break;
+                    case 'aufklarend':
+                        iconClass = 'fas fa-cloud-sun';
+                        break;
+                    case 'stark bewölkt':
+                        iconClass = 'fas fa-cloud';
+                        break;
+                    case 'halloween':
+                        iconClass = 'fas fa-ghost';
+                        break;
+                    default:
+                        iconClass = isDayTime ? 'fas fa-sun' : 'fas fa-moon';
+                        break;
+                }
+                document.getElementById('weather-icon').className = iconClass;
+
+                weatherIcon.classList.remove('fade-out');
+                weatherText.classList.remove('fade-out');
+                weatherIcon.classList.add('fade-in');
+                weatherText.classList.add('fade-in');
+            }, 500); 
         } else {
             document.getElementById('weather-text').innerText = '';
             document.getElementById('weather-icon').className = '';
         }
+
+        separators.forEach(separator => separator.style.display = 'none');
+        const visibleElements = [cityElement, timeElement, weatherElement, dateElement].filter(el => el.style.display !== 'none');
+        visibleElements.forEach((el, index) => {
+            if (index < visibleElements.length - 1) {
+                el.nextElementSibling.style.display = 'block';
+            }
+        });
 
         if (data.draggable) {
             makeDraggable(uiElement);
@@ -104,8 +140,47 @@ window.addEventListener('message', function(event) {
         const uiElement = document.getElementById('ui');
         uiElement.style.left = data.left;
         uiElement.style.top = data.top;
+    } else if (data.action === 'showAdminUI') {
+        const adminUI = document.getElementById('admin-ui');
+        adminUI.classList.remove('fade-out');
+        adminUI.classList.add('fade-in');
+        adminUI.style.display = 'block';
+        document.addEventListener('keydown', onKeyDown);
     }
 });
+
+document.getElementById('full-blackout').addEventListener('change', function() {
+    const fullBlackout = document.getElementById('full-blackout').checked;
+
+    fetch(`https://${GetParentResourceName()}/updateBlackout`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            fullBlackout: fullBlackout
+        })
+    });
+});
+
+function onKeyDown(e) {
+    if (e.key === 'Escape') {
+        const adminUI = document.getElementById('admin-ui');
+        adminUI.classList.remove('fade-in');
+        adminUI.classList.add('fade-out');
+        setTimeout(() => {
+            adminUI.style.display = 'none';
+            fetch(`https://${GetParentResourceName()}/closeAdminUI`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        }, 500); 
+        document.removeEventListener('keydown', onKeyDown);
+    }
+}
+
 
 function makeDraggable(element) {
     let isDragging = false;
